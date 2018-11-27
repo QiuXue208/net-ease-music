68a4b75b6c9ac2269f203786d0cd3b7c5781dd81
Snowy <1309571292@qq.com>
2018-11-27 14:28:48 +0800

    run build

---
+++ build/precache-manifest.069cd0dc5be087320badf224ba112922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3e453fb52e6f4502df6",
+    "url": "/static/js/main.d3e453fb.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d3e453fb52e6f4502df6",
+    "url": "/static/css/main.646e1d6b.chunk.css"
+  },
+  {
+    "revision": "15a749d6179768f0b101830f8cae3fa5",
+    "url": "/index.html"
+  }
+];
