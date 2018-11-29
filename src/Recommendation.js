d3daf3f8541cac7b4c9cc6ce8af37c74b57b0f08
Snowy <1309571292@qq.com>
2018-11-29 10:02:22 +0800

    add Recommendation component

---
+++ src/Recommendation.js
@@ -0,0 +1,15 @@
+import React from 'react' 
+import './App.css'
+
+class Recommendation extends React.PureComponent{
+    render(){
+        return (
+            <li>
+              <img src="http://p1.music.126.net/UFumsHtsuJky7gluwcC6Lg==/109951163683687709.webp?imageView&amp;thumbnail=247x0&amp;quality=75&amp;tostatic=0&amp;type=webp" />
+              <p>早知道做人这么累，当初就不下凡了</p>   
+            </li>
+        )
+    }
+}
+
+export default Recommendation
