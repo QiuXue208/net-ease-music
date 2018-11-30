2f643374b7c4bb50caa42ef2bc124c9b85c27c9f
Snowy <1309571292@qq.com>
2018-11-30 17:30:28 +0800

    add class

--- src/HotSong.js
+++ src/HotSong.js
@@ -4,7 +4,7 @@ import './App.css'
 class HotSong extends React.PureComponent{
     render(){
         return (
-            <div>
+            <div className="hotSong">
                 <div className="hotSongContainer">
                    <div className="hotSongIcon"></div>
                    <div className="updateDate">更新日期:11月30日</div>
