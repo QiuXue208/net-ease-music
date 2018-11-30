0f3af334f1a06e9c3187b3f2ffbce476ebf8b080
Snowy <1309571292@qq.com>
2018-11-30 17:31:21 +0800

    change constructor

--- src/App.js
+++ src/App.js
@@ -9,25 +9,40 @@ import HotSong from './HotSong'
 import Search from './Search'
 
 class App extends React.PureComponent {
+  constructor(){
+    super()
+    this.state ={
+      currentIndex:0
+    }
+  }
+  handleTabChange = (index)=>{
+   /* const targetTag = document.querySelector(`.showList > li:nth-child(${index + 1})`)
+    console.log(targetTag)
+    const data =  parseInt(targetTag.getAttribute('data-key'),10)*/
+    this.setState({
+      currentIndex:index
+    })
+    /*console.log('currentIndex' + this.state.currentIndex)*/
+  }
   render() {
     return (
       <div className="App">
         <div className="topBar">
           <Header />
-          <Nav />
+          <Nav  handleTabChange={this.handleTabChange}/>
         </div>
-        <ul className="showList">
-          <li className="show">
-             <RecommendationList />
-             <LatestSong />
-             <Footer />
-          </li>
-          <li className="non-show">
-             <HotSong />
-          </li>
-          <li className="non-show">
-             <Search />
-          </li>
+        <ul className="showList" >
+                    <li data-key="0" className={this.state.currentIndex === 0 ? 'show':'nonShow'}>
+                        <RecommendationList />
+                        <LatestSong />
+                        <Footer />
+                    </li>
+                    <li data-key="1" className={this.state.currentIndex === 1 ? 'show':'nonShow'}>
+                        <HotSong />
+                    </li>
+                    <li data-key="2" className={this.state.currentIndex === 2 ? 'show':'nonShow'}>
+                        <Search />
+                    </li>
         </ul>
       </div>
     );
--- src/Nav.js
+++ src/Nav.js
@@ -1,30 +1,40 @@
 import React from 'react'
 import './App.css'
 
-
 class Nav extends React.Component{
     constructor(){
         super()
         this.state = {
-            currentIndex:0
+            currentIndex:0,
         }
     }
     handleChangeStyle = (index)=>{
-           this.setState({
-               currentIndex:index
-           })
+            this.setState({
+                currentIndex:index
+            })
     }
     render(){
         const paragraphs = ['推荐音乐','热歌榜','搜索']
         const listItems = paragraphs.map((paragraph,index)=>{
-            return <li className={this.state.currentIndex===index?'active':'non-active'} onClick={()=>this.handleChangeStyle(index)}>{paragraph}</li> 
+            return (<li 
+                      key={index} 
+                      className={this.state.currentIndex===index?'active':'non-active'} 
+                      onClick={()=>{
+                          this.handleChangeStyle(index)
+                          this.props.handleTabChange(index)
+                        }
+                      } >
+                      {paragraph}
+                    </li>)
         })
         return (
-            <nav className="navbar">
-              <ul className="nav">
-                 {listItems}              
-              </ul>
-            </nav>
+            <div>
+                <nav className="navbar">
+                   <ul className="nav">
+                      {listItems}              
+                   </ul>
+                </nav>
+            </div>
         ) 
     }
 }
